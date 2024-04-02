82c226ea0543e587d792dc9193ecdfa09b788216
Nicolas Theau <theaun91@gmail.com>
2024-04-02 15:35:08 +0200

    Back Initial commit

---
+++ chatmqback/src/main/java/fr/efrei/rabbitmq/chatmq/ChatmqApplication.java
@@ -0,0 +1,13 @@
+package fr.efrei.rabbitmq.chatmq;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChatmqApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChatmqApplication.class, args);
+	}
+
+}
---
+++ chatmqback/src/test/java/fr/efrei/rabbitmq/chatmq/ChatmqApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.efrei.rabbitmq.chatmq;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChatmqApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
