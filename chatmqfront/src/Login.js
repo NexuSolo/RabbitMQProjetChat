3b3bcff15429e828d524675d73037a33e2e4b784
Yanis Rozier <yanis.rozier@orange.fr>
2024-04-03 15:53:22 +0200

    envoie valeur auth au back

--- chatmqfront/src/Login.js
+++ chatmqfront/src/Login.js
@@ -1,5 +1,4 @@
 import React, { useState } from 'react';
-import socketIOClient from 'socket.io-client';
 import './Login.css';
 
 const Login = ({ onLogin }) => {
@@ -7,35 +6,48 @@ const Login = ({ onLogin }) => {
     const [password, setPassword] = useState('');
 
     const handleLogin = () => {
-        const socket = socketIOClient('http://localhost:8080/auth/register');
-        socket.emit('login', { username });
-        onLogin();
+        const requestBody = JSON.stringify({ username, password });
+
+        fetch('http://localhost:8080/auth/register', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: requestBody
+        })
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('Login failed.');
+            }
+            return response.json();
+        })
+        .then(data => {
+            console.log(data);
+            onLogin();
+        })
+        .catch(error => {
+            console.error('Error:', error);
+        });
     };
 
     return (
-
         <div className='login'>
-
             <div className='login-logo'>
                 <img className='logo-img' src="message.png" alt="Logo" />
             </div>
-
             <div className='login-input'>
                 <div className='username-area'>
                     <h4>Nom d'utilisateur</h4>
                     <input type="text" placeholder="Exemple : Nexus" value={username} onChange={(e) => setUsername(e.target.value)} />
                 </div>
-
                 <div className='password-area'>
                     <h4>Mot de passe</h4>
                     <input type="password" placeholder="··············" value={password} onChange={(e) => setPassword(e.target.value)} />
                 </div>
             </div>
-
             <div className='login-button-area'>
                 <button className='login-button' onClick={handleLogin}>Se connecter</button>
             </div>
-
         </div>
     );
 };
