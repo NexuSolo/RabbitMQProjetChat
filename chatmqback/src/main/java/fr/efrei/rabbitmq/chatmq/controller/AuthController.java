96ea937376a79e1b2d81b284644cbd609cc2302f
Nicolas Theau <theaun91@gmail.com>
2024-04-03 16:30:40 +0200

    Ajout verification token

--- chatmqback/src/main/java/fr/efrei/rabbitmq/chatmq/controller/AuthController.java
+++ chatmqback/src/main/java/fr/efrei/rabbitmq/chatmq/controller/AuthController.java
@@ -5,6 +5,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,6 +13,9 @@ import com.nimbusds.jose.JOSEException;
 
 import fr.efrei.rabbitmq.chatmq.model.User;
 import fr.efrei.rabbitmq.chatmq.service.AuthentificationService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
 
 @RestController
 @RequestMapping("/auth/")
@@ -21,7 +25,7 @@ public class AuthController {
     private AuthentificationService authentificationService;
 
     @PostMapping("register")
-    public ResponseEntity<?> newUser(@RequestBody User user) {
+    public ResponseEntity<?> authentificate(@RequestBody User user) {
         try {
             if (!authentificationService.register(user)) {
                 if (!authentificationService.login(user)) {
@@ -36,4 +40,21 @@ public class AuthController {
             return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
         }
     }
+
+    @GetMapping("token")
+    public ResponseEntity<?> verifyToken(@RequestHeader String authorization) {
+        try {
+            if (authentificationService.verifyToken(authorization)) {
+                String username = authentificationService.getUsername(authorization);
+                return ResponseEntity.ok().body("{\"username\": \"" + username + "\"}");
+            }
+            else {
+                return new ResponseEntity<>(HttpStatus.UNAUTHORIZED);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+    
 }
